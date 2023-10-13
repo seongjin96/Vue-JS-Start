84cf37508a8b7d82ac75d332c2779a78d13017d3
seongjin96 <w8348@naver.com>
2023-10-13 10:09:33 +0900

    vue-cli

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Vue.js 공식 스타일 가이드에 나와 있는 컴포넌트 명명법 종류 -->
+
+    <!-- VisualStudioCode의 파일 바로가기의 기능을 활용하려면 아래 컴포넌트 명명법을 따라야 한다 -->
+    <hello-world></hello-world>
+    <!-- 파스칼 케이스(단어가 연결될때마다 단어의 첫 글자를 대문자로) -->
+    <HelloWorld></HelloWorld>
+    <!-- 조금 더 진보된 파스칼 케이스 -->
+    <HelloWorld/>
   </div>
 </template>
 
@@ -9,6 +17,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
   name: 'App',
   components: {
     HelloWorld
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -6,3 +6,10 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// // 위의 내용과 동일
+// new Vue({
+//   el: '#app',
+//   // App이라는 컴포넌트 내용을 렌더링
+//   render: h => h(App),
+// })
