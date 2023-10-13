8178827c1e7b9960ba851211f874644a26bbdbb9
seongjin96 <w8348@naver.com>
2023-10-13 11:19:55 +0900

    싱글 파일 컴포넌트

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,37 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- Vue.js 공식 스타일 가이드에 나와 있는 컴포넌트 명명법 종류 -->
-
-    <!-- VisualStudioCode의 파일 바로가기의 기능을 활용하려면 아래 컴포넌트 명명법을 따라야 한다 -->
-    <hello-world></hello-world>
-    <!-- 파스칼 케이스(단어가 연결될때마다 단어의 첫 글자를 대문자로) -->
-    <HelloWorld></HelloWorld>
-    <!-- 조금 더 진보된 파스칼 케이스 -->
-    <HelloWorld/>
+  <!-- template root는 하나의 element만 있어야 한다. -->
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
